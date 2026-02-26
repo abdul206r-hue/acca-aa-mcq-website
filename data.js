01d4658d44cefc96127d56a820b1486aa189057a
abdul206r-hue <abdul206r@gmail.com>
2026-02-26 21:08:17 +0000

    Created data.js file with MCQ database structure

---
+++ data.js
@@ -0,0 +1,4 @@
+const mcqDatabase = [  // Sample MCQ structure  {    question: 'What is the capital of France?',    options: ['Berlin', 'Madrid', 'Paris', 'Rome'],    answer: 'Paris',    explanation: 'Paris is the capital and most populous city of France.'  },  // Add more MCQs as needed];
+
+// Exporting the database structure to use in other modules
+module.exports = mcqDatabase;
